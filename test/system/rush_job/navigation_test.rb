1f4f789f92dc015107379a8f3a0a25124cd30e90
JavaKoala <javakoala1@gmail.com>
2023-09-09 13:06:39 -0400

    Highlight nav links

--- test/system/rush_job/navigation_test.rb
+++ test/system/rush_job/navigation_test.rb
@@ -4,26 +4,36 @@ module RushJob
   class NavigationTest < ApplicationSystemTestCase
     test 'root path' do
       visit '/rush_job/'
+
       assert_selector 'h2', text: 'Locked Jobs'
+      find_link('Dashboard', class: 'nav-link active')
+      find_link('Jobs', class: 'nav-link')
     end
 
     test 'navigate to jobs page' do
       visit '/rush_job/'
       click_link 'Jobs'
+
       assert_selector '#rush-job-jobs'
+      find_link('Dashboard', class: 'nav-link')
+      find_link('Jobs', class: 'nav-link active')
     end
 
     test 'navigate to dashboard' do
       visit '/rush_job/'
       click_link 'Jobs'
       click_link 'Dashboard'
+
       assert_selector 'h2', text: 'Locked Jobs'
+      find_link('Dashboard', class: 'nav-link active')
+      find_link('Jobs', class: 'nav-link')
     end
 
     test 'navigate to root path' do
       visit '/rush_job/'
       click_link 'Jobs'
       click_link 'Delayed Jobs'
+
       assert_selector 'h2', text: 'Locked Jobs'
     end
   end
