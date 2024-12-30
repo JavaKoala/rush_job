237845a714124b8bb9d35bf7fae9678e99d52fd7
JavaKoala <javakoala1@gmail.com>
2024-12-30 15:31:16 -0500

    Remove realtive JS references

--- app/assets/javascript/rush_job/application.js
+++ app/assets/javascript/rush_job/application.js
@@ -1,7 +1,7 @@
 import { Application } from '@hotwired/stimulus';
-import RushJobConfirmController from './controllers/rush_job_confirm_controller';
-import RushJobPollingController from './controllers/rush_job_polling_controller';
-import RushJobReloadJobsTableController from './controllers/rush_job_reload_jobs_table_controller';
+import RushJobConfirmController from 'rush_job/controllers/rush_job_confirm_controller';
+import RushJobPollingController from 'rush_job/controllers/rush_job_polling_controller';
+import RushJobReloadJobsTableController from 'rush_job/controllers/rush_job_reload_jobs_table_controller';
 import 'bootstrap';
 import '@hotwired/turbo-rails';
 
