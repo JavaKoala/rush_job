53ec0d843f80ac15f0c4a524fea76251b05c393c
JavaKoala <javakoala1@gmail.com>
2025-02-11 16:27:00 -0500

    Update theme

--- test/system/rush_job/theme_test.rb
+++ test/system/rush_job/theme_test.rb
@@ -5,31 +5,37 @@ module RushJob
     test 'changing modes' do
       visit '/rush_job'
 
-      assert_css '.background-color-light'
+      assert_css 'body[data-bs-theme=light]'
 
       click_link_or_button 'Options'
       click_link_or_button 'Dark Mode'
 
-      assert_css '.background-color-dark'
+      assert_css 'body[data-bs-theme=dark]'
       assert_no_button 'Dark Mode'
-      assert_no_css '.background-color-light'
+      assert_no_css 'body[data-bs-theme=light]'
+
       click_link_or_button 'Options'
+
       assert_button 'Light Mode'
 
       refresh
 
-      assert_css '.background-color-dark'
+      assert_css 'body[data-bs-theme=dark]'
       assert_no_button 'Dark Mode'
-      assert_no_css '.background-color-light'
+      assert_no_css 'body[data-bs-theme=light]'
+
       click_link_or_button 'Options'
+
       assert_button 'Light Mode'
 
       click_link_or_button 'Light Mode'
 
-      assert_no_css '.background-color-dark'
+      assert_no_css 'body[data-bs-theme=dark]'
+
       click_link_or_button 'Options'
+
       assert_button 'Dark Mode'
-      assert_css '.background-color-light'
+      assert_css 'body[data-bs-theme=light]'
       assert_no_button 'Light Mode'
     end
   end
